50c9391b6cf281c1440c723e4011fa9216dfc67a
Aaron Stanley King <trendoid@gmail.com>
2024-03-30 15:15:29 -0400

    fixed accessibility heading error

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -6,9 +6,9 @@ export function Intro() {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Aaron Stanley King
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
         We'll figure it all out in the end.
-      </h4>
+      </h2>
     </section>
   );
 }
