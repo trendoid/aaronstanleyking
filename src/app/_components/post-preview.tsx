5412385b526f7f79a0c4314cbdbe07c176d248ff
Aaron Stanley King <trendoid@gmail.com>
2024-03-30 15:25:30 -0400

    fixed console error

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -29,7 +29,7 @@ export function PostPreview({
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
           as={`/posts/${slug}`}
-          href="/posts/[slug]"
+          href="/posts/"
           className="hover:underline"
         >
           {title}
